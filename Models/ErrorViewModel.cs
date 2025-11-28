f5668a6004c7a754b131f396b5152eb0c409aea1
hih0lp <mrlibobo@yandex.ru>
2025-11-28 21:26:36 +0300

    commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PSB_HACKATHON.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
